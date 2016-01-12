e76c185cb0257d9b93c9058a2dc0f41cf380e0fe
Leonardo Marques <leommarques7@gmail.com>
2016-01-12 18:12:05 -0200

    fundo branco nas telas

--- app/src/main/java/br/ufrj/caronae/App.java
+++ app/src/main/java/br/ufrj/caronae/App.java
@@ -55,9 +55,9 @@ public class App extends SugarApp {
     public static NetworkService getNetworkService() {
         if (networkService == null) {
             //String endpoint = MEUDIGOCEAN_DEV_ENDPOINT;
-            String endpoint = MEUDIGOCEAN_PROD_ENDPOINT;
+            //String endpoint = MEUDIGOCEAN_PROD_ENDPOINT;
             //String endpoint = LOCAL_SERV_ENDPOINT;
-            //String endpoint = TIC_ENDPOINT;
+            String endpoint = TIC_ENDPOINT;
 
             networkService = new RestAdapter.Builder()
                     .setEndpoint(endpoint)
