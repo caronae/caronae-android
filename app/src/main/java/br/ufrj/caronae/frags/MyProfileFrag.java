68945691495ca3bec4527bdcd0c9a37427af1806
Leonardo Marques <leommarques7@gmail.com>
2016-01-18 04:26:14 -0200

    bug fix edittext profile

--- app/src/main/java/br/ufrj/caronae/frags/MyProfileFrag.java
+++ app/src/main/java/br/ufrj/caronae/frags/MyProfileFrag.java
@@ -256,7 +256,16 @@ public class MyProfileFrag extends Fragment {
     }
 
     private boolean validatePhone(String phone) {
-        return phone.length() == 11;
+        if (phone.length() == 11) {
+            for (int i = 0; i < phone.length() && i < 11; i++) {
+                if (!Character.isDigit(phone.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
     }
 
     private void fillUserFields(User user) {
