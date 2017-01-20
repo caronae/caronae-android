ca9fe080afccc656219786b1e0eb68cffc53f72d
luisfnicolau <luisfnicolau@hotmail.com>
2017-01-20 12:35:30 -0200

    Implementado abrir chat quando clica na notificação

--- app/src/main/java/br/ufrj/caronae/acts/MainAct.java
+++ app/src/main/java/br/ufrj/caronae/acts/MainAct.java
@@ -65,6 +65,7 @@ public class MainAct extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         SharedPref.setChatActIsForeground(false);
--- app/src/main/java/br/ufrj/caronae/acts/StartAct.java
+++ app/src/main/java/br/ufrj/caronae/acts/StartAct.java
@@ -3,17 +3,52 @@ package br.ufrj.caronae.acts;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.Set;
 
 import br.ufrj.caronae.App;
 
 public class StartAct extends AppCompatActivity {
 
+    private final String RIDE_ID_BUNDLE_KEY     = "rideId";
+
+    private final String MSG_TYPE_BUNDLE_KEY    = "msgType";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*
+        Key Set received from Notification:
+        google.sent_time
+        rideId
+        from
+        google.message_id
+        message
+        senderId
+        msgType
+        collapse_key
+        */
+
+        Bundle bundle = getIntent().getExtras();
+
+        if(bundle!=null) {
+            for (String key : bundle.keySet()) {
+                Object value = bundle.get(key);
+                Log.d("DATA_SENT", String.format("%s %s (%s)", key,
+                        value.toString(), value.getClass().getName()));
+            }
+        }
+
         if (App.isUserLoggedIn())
-            startActivity(new Intent(this, MainAct.class));
+            if(bundle != null && bundle.get(MSG_TYPE_BUNDLE_KEY).equals("chat")){
+                Intent intent = new Intent(this, ChatAct.class);
+                intent.putExtra(RIDE_ID_BUNDLE_KEY, (String)bundle.get(RIDE_ID_BUNDLE_KEY));
+                startActivity(intent);
+            } else {
+                startActivity(new Intent(this, MainAct.class));
+            }
         else
             startActivity(new Intent(this, OpeningAct.class));
 
