ec7ab789e12049706ac66907d19dd3d2fd4a6ec6
Luis <luisfnicolau@hotmail.com>
2017-02-22 15:58:45 -0300

    Adicionado FAQ no menu lateral

---
+++ app/src/main/java/br/ufrj/caronae/frags/FAQFrag.java
@@ -0,0 +1,37 @@
+package br.ufrj.caronae.frags;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import br.ufrj.caronae.App;
+import br.ufrj.caronae.R;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class FAQFrag extends Fragment {
+
+    @Bind(R.id.webview_faq)
+    WebView webView;
+
+
+    public FAQFrag() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_faq, container, false);
+        ButterKnife.bind(this, view);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(App.getHost() + "static_pages/faq.html");
+
+        return view;
+    }
+}
