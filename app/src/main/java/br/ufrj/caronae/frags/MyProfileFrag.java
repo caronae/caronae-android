e3d5caeddd7b173d9416a5a6fcf9909d65efa4b7
Luis <luisfnicolau@hotmail.com>
2017-08-27 12:19:59 -0300

    Removida opcao de escolher foto do Siga

--- app/src/main/java/br/ufrj/caronae/frags/MyProfileFrag.java
+++ app/src/main/java/br/ufrj/caronae/frags/MyProfileFrag.java
@@ -401,8 +401,10 @@ public class MyProfileFrag extends Fragment {
             }
         };
 
-        builder.items(new String[]{getString(R.string.frag_myprofile_SigaPicChoice), getString(R.string.frag_myprofile_facePicChoice)}, 0)
-                .title(getString(R.string.frag_myprofile_picChoice))
+//        builder.items(new String[]{getString(R.string.frag_myprofile_SigaPicChoice), getString(R.string.frag_myprofile_facePicChoice)}, 0)
+        builder.items(new String[]{getString(R.string.frag_myprofile_facePicChoice)}, 0)
+//                .title(getString(R.string.frag_myprofile_picChoice))
+                .title("Usar foto do Facebook?")
                 .positiveAction(getString(R.string.ok))
                 .negativeAction(getString(R.string.cancel));
         DialogFragment fragment = DialogFragment.newInstance(builder);
