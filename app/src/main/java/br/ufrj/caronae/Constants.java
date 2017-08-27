5d6ff923c5117c477ebd37119a26d95e6a0217b7
Luis <luisfnicolau@hotmail.com>
2017-08-27 12:08:47 -0300

    Adicionado compartilhamento de caronas e acesso a caronas por link

---
+++ app/src/main/java/br/ufrj/caronae/Constants.java
@@ -0,0 +1,14 @@
+package br.ufrj.caronae;
+
+/**
+ * Created by Luis on 8/26/2017.
+ */
+
+public class Constants {
+
+    public static final String DEV_SERVER_ENDPOINT          = "https://dev.caronae.tic.ufrj.br/";
+    public static final String TIC_ENDPOINT                 = "https://api.caronae.ufrj.br/";
+    public static final String CARONAE_ENDPOINT             = "https://api.caronae.com.br/";
+    public static final String SHARE_LINK                   = "https://caronae.com.br/carona/";
+
+}
