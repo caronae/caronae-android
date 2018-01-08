d54984e9ebe685ad2b5fb9695fdd17f86685f614
Luis Felipe Nicolau <luisfnicolau@hotmail.com>
2018-01-08 17:51:10 -0200

    Mudado cor das tabs

--- app/src/main/java/br/ufrj/caronae/frags/TabbedRideOfferFrag.java
+++ app/src/main/java/br/ufrj/caronae/frags/TabbedRideOfferFrag.java
@@ -3,11 +3,13 @@ package br.ufrj.caronae.frags;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import br.ufrj.caronae.App;
 import br.ufrj.caronae.R;
 import br.ufrj.caronae.adapters.RideDirectionFragmentPagerAdapter;
 import butterknife.BindView;
@@ -30,6 +32,23 @@ public class TabbedRideOfferFrag extends Fragment {
         viewPager.setAdapter(new RideDirectionFragmentPagerAdapter(getChildFragmentManager(), RideOfferFrag.class,  getResources().getStringArray(R.array.tab_tags)));
         tabLayout.setupWithViewPager(viewPager);
 
+        tabLayout.setBackground(ContextCompat.getDrawable(App.inst(), R.drawable.transparency_gradient_top_botton));
+
+        configureTabIndicators();
+
         return view;
+    }
+
+    private void configureTabIndicators() {
+
+        View tab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(0);
+        ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) tab.getLayoutParams();
+        p.setMargins(25, 0, 25, 0);
+        tab.requestLayout();
+
+        tab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(1);
+        p = (ViewGroup.MarginLayoutParams) tab.getLayoutParams();
+        p.setMargins(25, 0, 25, 0);
+        tab.requestLayout();
     }
 }
