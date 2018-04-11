8d197f12a0d7f2318e945c1e454a422b6f2f0257
Breno Ferreira Rocha <brenoferreirarocha@gmail.com>
2018-04-11 16:38:43 -0300

    Adiciona bottom navigation view

--- app/src/main/java/br/ufrj/caronae/acts/MainAct.java
+++ app/src/main/java/br/ufrj/caronae/acts/MainAct.java
@@ -1,12 +1,17 @@
 package br.ufrj.caronae.acts;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import android.widget.TextView;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Typeface;
-import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -16,7 +21,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
@@ -79,11 +83,31 @@ public class MainAct extends AppCompatActivity {
 
     private ArrayList<Class> backstack;
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_allrides:
+                    return true;
+                case R.id.navigation_myrides:
+                    return true;
+                case R.id.navigation_menu:
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         filterCard = (CardView) findViewById(R.id.filter_card);
         filterText = (TextView) findViewById(R.id.filter_text);
@@ -226,7 +250,7 @@ public class MainAct extends AppCompatActivity {
         if (!SharedPref.FRAGMENT_INDICATOR.equals("")){
             if (SharedPref.FRAGMENT_INDICATOR.equals(MyRidesFrag.class.getName()))
                 SharedPref.FRAGMENT_INDICATOR = "";
-                showActiveRidesFrag();
+            showActiveRidesFrag();
         }
     }
 
