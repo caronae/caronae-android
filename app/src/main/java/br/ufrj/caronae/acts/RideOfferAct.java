f6f17808acb4b7c72ef06c44f91d8477498e0e22
Breno Ferreira Rocha <brenoferreirarocha@gmail.com>
2018-04-17 16:34:31 -0300

    Adiciona bordas redondas ao botão de "Pegar Carona" e remove negrito de alguns títulos

--- app/src/main/java/br/ufrj/caronae/acts/RideOfferAct.java
+++ app/src/main/java/br/ufrj/caronae/acts/RideOfferAct.java
@@ -4,6 +4,12 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -32,6 +38,7 @@ import java.util.List;
 import br.ufrj.caronae.App;
 import br.ufrj.caronae.R;
 import br.ufrj.caronae.RoundedTransformation;
+import br.ufrj.caronae.SharedPref;
 import br.ufrj.caronae.SwipeDismissBaseActivity;
 import br.ufrj.caronae.Util;
 import br.ufrj.caronae.frags.AllRidesFrag;
@@ -43,6 +50,7 @@ import br.ufrj.caronae.models.User;
 import br.ufrj.caronae.models.modelsforjson.RideForJson;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -64,7 +72,7 @@ public class RideOfferAct extends SwipeDismissBaseActivity {
     @BindView(R.id.time_dt)
     public TextView time_dt;
     @BindView(R.id.join_bt)
-    public Button join_bt;
+    public android.widget.Button join_bt;
     @BindView(R.id.way_dt)
     public TextView way_dt;
     @BindView(R.id.place_dt)
@@ -77,6 +85,8 @@ public class RideOfferAct extends SwipeDismissBaseActivity {
     ImageButton shareButton;*/
     @BindView(R.id.title_lay)
     RelativeLayout locationBackground;
+    @BindView(R.id.back_bt)
+    TextView back_bt;
 
     RideForJson rideWithUsers;
 
@@ -219,8 +229,6 @@ public class RideOfferAct extends SwipeDismissBaseActivity {
         }
 
         AllRidesFrag.setPageThatWas(rideWithUsers.isGoing());
-
-
         final User driver = rideWithUsers.getDriver();
 
         final boolean isDriver = driver.getDbId() == App.getUser().getDbId();
@@ -228,7 +236,14 @@ public class RideOfferAct extends SwipeDismissBaseActivity {
         photo_iv = (CircleImageView)user_pic;
 
         int color = Util.getColorbyZone(rideWithUsers.getZone());
-        join_bt.setBackgroundColor(color);
+        Drawable background = join_bt.getBackground();
+        if (background instanceof ShapeDrawable) {
+            ((ShapeDrawable)background).getPaint().setColor(color);
+        } else if (background instanceof GradientDrawable) {
+            ((GradientDrawable)background).setColor(color);
+        } else if (background instanceof ColorDrawable) {
+            ((ColorDrawable)background).setColor(color);
+        }
         locationBackground.setBackgroundColor(color);
         photo_iv.setBorderColor(color);
 
@@ -262,7 +277,14 @@ public class RideOfferAct extends SwipeDismissBaseActivity {
         });
         location_dt.setText(location);
         name_dt.setText(driver.getName());
-        String info = driver.getProfile() + " | " + driver.getCourse();
+        String info;
+        if(driver.getProfile().equals("Graduação")) {
+            info = driver.getProfile() + " | " + driver.getCourse();
+        }
+        else
+        {
+            info = driver.getProfile();
+        }
         profile_dt.setText(info);
         if (rideWithUsers.getRoute().equals("")) {
             way_dt.setText("- - -");
@@ -283,8 +305,8 @@ public class RideOfferAct extends SwipeDismissBaseActivity {
         else
             dateDescription = getString(R.string.leavingAt, Util.formatTime(rideWithUsers.getTime()));
 
-        dateDescription = dateDescription + " | " + getWeekDayFromDate(Util.formatBadDateWithYear(rideWithUsers.getDate())) + " | " +Util.formatBadDateWithoutYear(rideWithUsers.getDate());
-        clock.setColorFilter(color, android.graphics.PorterDuff.Mode.MULTIPLY);
+        dateDescription = dateDescription + " | " + getWeekDayFromDate(rideWithUsers.getDate()) + " | " +Util.formatBadDateWithoutYear(rideWithUsers.getDate());
+        clock.setColorFilter(color, PorterDuff.Mode.SRC_IN);
         time_dt.setText(dateDescription);
         time_dt.setTextColor(color);
         if (rideWithUsers.getDescription().equals("")) {
@@ -457,6 +479,7 @@ public class RideOfferAct extends SwipeDismissBaseActivity {
     }
     public static String getWeekDayFromDate(String dateString) {
         int dayOfWeekInt = -1;
+        Log.e("LOGGING: ", dateString);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         try {
             Calendar c = Calendar.getInstance();
@@ -495,4 +518,14 @@ public class RideOfferAct extends SwipeDismissBaseActivity {
         }
         return dayOfWeek;
     }
+
+    @OnClick(R.id.back_bt)
+    public void backToMain()
+    {
+        SharedPref.NAV_INDICATOR = "AllRides";
+        Intent intent = new Intent(this, MainAct.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.anim_left_slide_in, R.anim.anim_right_slide_out);
+    }
+
 }
