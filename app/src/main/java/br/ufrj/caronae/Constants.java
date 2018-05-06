f3b0bf7c0c3720206b971f43f64040c4fe1ae27e
Breno Ferreira Rocha <brenoferreirarocha@gmail.com>
2018-05-06 14:06:17 -0300

    Configura ambiente dev para o compartilhamento de caronas

--- app/src/main/java/br/ufrj/caronae/Constants.java
+++ app/src/main/java/br/ufrj/caronae/Constants.java
@@ -1,15 +1,10 @@
 package br.ufrj.caronae;
 
-/**
- * Created by Luis on 8/26/2017.
- */
-
 public class Constants {
 
-    public static final String API_DEV_BASE_URL             = "https://api.dev.caronae.org/";
-    public static final String API_PRODUCTION_BASE_URL      = "https://api.caronae.org/";
-    public static final String CARONAE_URL_BASE             = "https://caronae.org/";
+    private static final String BUILD_TYPE                  = "prod";
+    public static final String API_BASE_URL                 = BUILD_TYPE.equals("prod")?"https://api.caronae.org/":"https://api.dev.caronae.org/";
+    public static final String CARONAE_URL_BASE             = BUILD_TYPE.equals("prod")?"https://caronae.org/":"https://dev.caronae.org/";
     public static final String SHARE_LINK                   = CARONAE_URL_BASE + "carona/";
 
 }
-
